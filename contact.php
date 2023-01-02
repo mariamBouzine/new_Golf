7ea07b163c767acb55c841d5fb83fd2276d58fa7
mariamBouzine <92028612+mariamBouzine@users.noreply.github.com>
2023-01-03 00:24:23 +0100

    add  page cours

---
+++ contact.php
@@ -0,0 +1,14 @@
+<section class="contact">
+        <h2 data-aos="fade-up" class="contact_title">NT Golf Academy</h2>
+        <h2 data-aos="fade-up" class="contact_sous_titre">Nos valeurs</h2>
+        <p data-aos="fade-up" class="paragraph_contact">orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+        <div data-aos="fade-up" class="d-flex justify-content-center mt-5">
+            <div class="frm-concact">
+                <span class="wpcf7-form-control-wrap" data-name="mail">
+                    <input type="email" name="mail" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email nl_email" aria-required="true" aria-invalid="false" placeholder="E-mail">
+                </span><br>
+                <input type="submit" value="Decouvrir" class="btn_contact wpcf7-form-control has-spinner wpcf7-submit nl_btns">
+                <span class="wpcf7-spinner"></span>
+            </div>
+        </div>
+    </section>
---
+++ cours.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include("nav.php"); ?>
+    <header>
+        <div class="header">
+            <div class="slideshow-container">
+                <img src="img/header_cours.png" alt="image_header" />
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="title aos-init  " data-aos="fade-up" data-aos-duration="2500">
+                            <h1 class="aos-init " data-aos="fade-up" data-aos-duration="2500"> NT Golf Academy</h1>
+                            <h3 data-aos="fade-up">Cours</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="box_Decouvrir" data-aos="fade-up" class="box_Cours">
+            <h1>Nos Cours</h1>
+        </div>
+    </header>
+    <div class="arrow_link">
+        <div class="arrow bounce">
+            <a href="#box_Decouvrir" aria-label="scroll_down">
+                <svg width="8" height="61" viewBox="0 0 8 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.64645 60.3536C3.84171 60.5488 4.15829 60.5488 4.35355 60.3536L7.53553 57.1716C7.7308 56.9763 7.7308 56.6597 7.53553 56.4645C7.34027 56.2692 7.02369 56.2692 6.82843 56.4645L4 59.2929L1.17157 56.4645C0.976311 56.2692 0.659728 56.2692 0.464466 56.4645C0.269204 56.6597 0.269204 56.9763 0.464466 57.1716L3.64645 60.3536ZM4.5 60L4.5 0H3.5L3.5 60H4.5Z" fill="white"></path>
+                </svg>
+            </a>
+        </div>
+    </div>
+    <section class="box_typeCours">
+        <div class="d-flex type_Cours">
+            <div class="flex-shrink-0 numbr_cour">
+                <h1>01</h1>
+            </div>
+            <div class="flex-shrink-1 img_cour">
+                <img src="./img/cours.png" alt="...">
+            </div>
+            <div class="flex-grow-2 ms-3 text_cour">
+                <h2>Cours débutants</h2>
+                <p>Nos cours de golf sont disponibles 365 jours par an.
+                    Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                    Il vous est loisible de choisir entre différentes catégories de cours :
+                </p>
+                <div class="d-flex box_icons_cours">
+                    <a href="#"><img src="./img/icons/icon_cour.png" /></a>
+                    <a href="#"><img src="./img/icons/icon_star.png" /></a>
+                </div>
+            </div>
+            <div class="flex-grow-3 ms-3 btn_icon_cour">
+                <a href=""><img src="./img/icons/btnicon_cour.png" /></a>
+            </div>
+        </div>
+        <div class="d-flex type_Cours">
+            <div class="flex-shrink-0 numbr_cour">
+                <h1>02</h1>
+            </div>
+            <div class="flex-shrink-1 img_cour">
+                <img src="./img/cours.png" alt="...">
+            </div>
+            <div class="flex-grow-2 ms-3 text_cour">
+                <h2>Cours mens</h2>
+                <p>NNos cours de golf sont disponibles 365 jours par an.
+                    Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                    Il vous est loisible de choisir entre différentes catégories de cours :
+                </p>
+                <div class="d-flex box_icons_cours">
+                    <a href="#"><img src="./img/icons/icon_cour.png" /></a>
+                    <a href="#"><img src="./img/icons/icon_star.png" /></a>
+                </div>
+            </div>
+            <div class="flex-grow-3 ms-3 btn_icon_cour">
+                <a href=""><img src="./img/icons/btnicon_cour.png" /></a>
+            </div>
+        </div>
+        <div class="d-flex type_Cours">
+            <div class="flex-shrink-0 numbr_cour">
+                <h1>03</h1>
+            </div>
+            <div class="flex-shrink-1 img_cour">
+                <img src="./img/cours.png" alt="...">
+            </div>
+            <div class="flex-grow-2 ms-3 text_cour">
+                <h2>Cours ladies</h2>
+                <p>Nos cours de golf sont disponibles 365 jours par an.
+                    Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                    Il vous est loisible de choisir entre différentes catégories de cours :
+                </p>
+                <div class="d-flex box_icons_cours">
+                    <a href="#"><img src="./img/icons/icon_cour.png" /></a>
+                    <a href="#"><img src="./img/icons/icon_star.png" /></a>
+                </div>
+            </div>
+            <div class="flex-grow-3 ms-3 btn_icon_cour">
+                <a href=""><img src="./img/icons/btnicon_cour.png" /></a>
+            </div>
+        </div>
+        <div class="d-flex type_Cours">
+            <div class="flex-shrink-0 numbr_cour">
+                <h1>04</h1>
+            </div>
+            <div class="flex-shrink-1 img_cour">
+                <img src="./img/cours.png" alt="...">
+            </div>
+            <div class="flex-grow-2 ms-3 text_cour">
+                <h2>Cours séniors</h2>
+                <p>Nos cours de golf sont disponibles 365 jours par an.
+                    Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                    Il vous est loisible de choisir entre différentes catégories de cours :
+                </p>
+                <div class="d-flex box_icons_cours">
+                    <a href="#"><img src="./img/icons/icon_cour.png" /></a>
+                    <a href="#"><img src="./img/icons/icon_star.png" /></a>
+                </div>
+            </div>
+            <div class="flex-grow-3 ms-3 btn_icon_cour">
+                <a href=""><img src="./img/icons/btnicon_cour.png" /></a>
+            </div>
+        </div>
+    </section>
+    <?php include("contact.php");
+          include("footer.php")
+    ?>
+</body>
+
+</html>
