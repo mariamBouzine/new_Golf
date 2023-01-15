78a9fa21a6235038003fc11f0e994e518e2d8294
mariamBouzine <92028612+mariamBouzine@users.noreply.github.com>
2023-01-15 21:21:05 +0100

    new

---
+++ detail_cour.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="./js/script.js"></script>
+
+<body>
+    <?php include("nav.php"); ?>
+    <header>
+        <div class="header">
+            <div class="slideshow-container">
+                <img src="img/header_cours.png" alt="image_header" />
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="title aos-init  " data-aos="fade-up" data-aos-duration="2500">
+                            <h1 class="aos-init " data-aos="fade-up" data-aos-duration="2500"> NT Golf Academy</h1>
+                            <h3 data-aos="fade-up">Cours</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="box_Decouvrir" data-aos="fade-up" class="box_Cours">
+            <h1>Nos Cours</h1>
+        </div>
+    </header>
+    <div class="container box_Deposit">
+        <div class="row">
+            <div class="col-7">
+                <div>
+                    <p>Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                        l vous est loisible de choisir entre différentes catégories de cours :</p>
+                    <p class="paragraph1">hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                        l vous est loisible de choisir entre différentes catégories de cours :
+                        Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.</p>
+                    <p class="paragraph1">l vous est loisible de choisir entre différentes catégories de cours :
+                        Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités. vous est loisible de choisir entre différentes catégories de cours :
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <a class="Deposit-button" href=""><strong>Low Deposit </strong>- Flexibility Guarantee <img src="./img/icons/next.png"></a>
+            </div>
+        </div><br>
+        <p><strong>Please note that a Golf Buggy or Caddy is compulsory for playing on the course. LOW DEPOSIT + Flexibility Guarantee</strong></p>
+    </div>
+    <section class="box_Offers">
+        <div class="container ">
+            <div class="row">
+                <div class="col-7">
+                    <h2>Top Offers at Golf paris</h2>
+                    <div class="box_date">
+                        <label for="">Arriving on</label>
+                        <input type="text" placeholder="Any date">
+                    </div>
+                    <p>We found 0 offers that match your search</p>
+                    <div class="_box_contact">
+                        <div class="_contact">
+                            <p id="paragraph_top">Sorry, we can't find a package for PalmGolf Paris on your selected date</p>
+                            <p>Please try another date, or alternatively, call our Golf Experts who can create a fully customised package for you for any date in less than 60 seconds!</p>
+                            <a class="tele_button" href=""><img src="./img/icons/tele.png" />06 000 000 000</a>
+                            <p id="paragraph_buttom">Not available to chat? Request a call back</p>
+                        </div>
+                        <div class="contact_line"></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box_holiday">
+                        <p class="title_holiday">Create your own holiday</p>
+                        <form class="form_holiday">
+                            <div class="input_form">
+                                <label for="">Arriving on</label>
+                                <input type="text" placeholder="Any date">
+                            </div>
+                            <div class="input_form">
+                                <label for="">Group size</label>
+                                <select class="form-select selectdiv" aria-label="Default select example">
+                                    <option>1</option>
+                                    <option value="1">2</option>
+                                    <option value="2">3</option>
+                                    <option value="3">4</option>
+                                </select>
+                            </div>
+                            <div class="input_form">
+                                <label for="">Number of nights</label>
+                                <select class="form-select selectdiv" aria-label="Default select example">
+                                    <option>1</option>
+                                    <option value="1">2</option>
+                                    <option value="2">3</option>
+                                    <option value="3">4</option>
+                                </select>
+                            </div>
+                            <div class="input_form">
+                                <label for="">Number of rounds</label>
+                                <select class="form-select selectdiv" aria-label="Default select example">
+                                    <option>1</option>
+                                    <option value="1">2</option>
+                                    <option value="2">3</option>
+                                    <option value="3">4</option>
+                                </select>
+                            </div>
+                            <div class="form-check">
+                                <input class="" type="checkbox" value="" id="flexCheckChecked" checked>
+                                <label class="form-check-label" for="flexCheckChecked">
+                                    Yes, I'm interested in flight options
+                                </label>
+                            </div>
+                            <select class="form-select Edinburgh" aria-label="Default select example">
+                                <option class="selected">Edinburgh</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                            <a class="holiday_button" href="">CREATE MY ENQUIRY<img src="./img/icons/next.png" /></a>
+                            <p id="holiday_parag">Speak with someone who's been there! Call us on <br>
+                                06 000 000 000until <b>7PM</b> today</p>
+                        </form>
+                    </div>
+                    <div class="holiday_line"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container box_Deposit">
+        <div class="row">
+            <div class="col-7">
+                <h2>Top Offers at Golf paris</h2>
+                <div>
+                    <p>Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                        l vous est loisible de choisir entre différentes catégories de cours :</p>
+                    <p class="paragraph1">hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.
+                        l vous est loisible de choisir entre différentes catégories de cours :
+                        Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.</p>
+                    <p class="paragraph1">l vous est loisible de choisir entre différentes catégories de cours :
+                        Nos cours de golf sont disponibles 365 jours par an.
+                        Que vous soyez débutants, confirmés, juniors, séniors, hommes ou dames, nous adaptons Notre pédagogie et notre enseignement à vos besoins et vos capacités.</p>
+                </div>
+            </div>
+            <div class="col">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div id="map"></div>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly" defer></script>
+    </div>
+    <div class="container">
+        <div class="box_Reviews">
+            <div class="title_Reviews">
+                <h2>Course Reviews</h2>
+                <p>Sort By:</p>
+            </div>
+            <div>
+                <a class="REVIEW_button" href=""><img src="./img/icons/review.png" />WRITE A REVIEW</a>
+            </div>
+        </div>
+    </div>
+    <section class="section_Reviews">
+        <div class="_Reviews">
+            <div class="container ">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <p class="_Reviews_date">May. 29, 2022</p>
+                            <div class="evaluation">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <p>6.0</p>
+                            </div>
+                            <div class="evaluation_person">
+                                <label class="name_per" for="">A</label>
+                                <label class="lname_per" for="">************</label>
+                            </div>
+                            <div class="box_like">
+                                <div class="like">
+                                    <a href=""><img src="./img/icons/like.png" /></a>
+                                </div>
+                                <div class="dislike">
+                                    <a href=""><img src="./img/icons/dislike.png" /></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col line_v"></div>
+                    <div class="col-8">
+                        <div class="box_comments">
+                            <h2>Top Offers at Golf paris</h2>
+                            <p>The views. The holes themselves. This is a must play anytime on a trip to Ireland/ Northern Ireland.</p>
+                            <a href="">Show More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="_Reviews_line"></div>
+        <div class="_Reviews">
+            <div class="container ">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <p class="_Reviews_date">May. 29, 2022</p>
+                            <div class="evaluation">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <p>6.0</p>
+                            </div>
+                            <div class="evaluation_person">
+                                <label class="name_per" for="">A</label>
+                                <label class="lname_per" for="">************</label>
+                            </div>
+                            <div class="box_like">
+                                <div class="like">
+                                    <a href=""><img src="./img/icons/like.png" /></a>
+                                </div>
+                                <div class="dislike">
+                                    <a href=""><img src="./img/icons/dislike.png" /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col line_v"></div>
+                    <div class="col-8">
+                        <div class="box_comments">
+                            <h2>Top Offers at Golf paris</h2>
+                            <p>The views. The holes themselves. This is a must play anytime on a trip to Ireland/ Northern Ireland.</p>
+                            <a href="">Show More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="_Reviews_line"></div>
+        <div class="_Reviews">
+            <div class="container ">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <p class="_Reviews_date">May. 29, 2022</p>
+                            <div class="evaluation">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <img src="./img/icons/ball.png" alt="">
+                                <p>6.0</p>
+                            </div>
+                            <div class="evaluation_person">
+                                <label class="name_per" for="">A</label>
+                                <label class="lname_per" for="">************</label>
+                            </div>
+                            <div class="box_like">
+                                <div class="like">
+                                    <a href=""><img src="./img/icons/like.png" /></a>
+                                </div>
+                                <div class="dislike">
+                                    <a href=""><img src="./img/icons/dislike.png" /></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col line_v"></div>
+                    <div class="col-8">
+
+                        <div class="box_comments">
+                            <h2>Top Offers at Golf paris</h2>
+                            <p>The views. The holes themselves. This is a must play anytime on a trip to Ireland/ Northern Ireland.</p>
+                            <a href="">Show More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="_Reviews_line"></div>
+    </section>
+    <?php
+    include("footer.php");
+    ?>
+</body>
+
+</html>
